c9286a3de1061b4f314243f3dc06dc8477c311b1
SMAILe3001 <smaile3001@gmail.com>
2023-02-28 20:41:25 +0200

    The second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsGeneration(galleryItems));
+
+function galleryItemsGeneration(galleryItems) {
+	return galleryItems
+		.map(
+			({ preview, description, original }) => `
+            <div class="gallery__item">
+				<a class="gallery__link" href="${original}">
+					<img
+					class="gallery__image"
+					src="${preview}"
+					alt="${description}"
+					/>
+				</a>
+			</div>`,
+		)
+		.join("");
+}
+
+galleryEl.addEventListener("click", lightbox);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+	scrollZoom: false,
+});
