d0d43b37457bd45b9c92fb19c85909be6e182c6f
SMAILe3001 <smaile3001@gmail.com>
2023-02-28 19:04:56 +0200

    first task final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,13 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryEl = document.querySelector('.gallery');
-galleryEl.insertAdjacentHTML('beforeend', galleryItemsGeneration(galleryItems));
+const galleryEl = document.querySelector(".gallery");
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsGeneration(galleryItems));
 
 function galleryItemsGeneration(galleryItems) {
-  return galleryItems
-    .map(({ preview, description, original }) => {
-      return `
+	return galleryItems
+		.map(
+			({ preview, description, original }) => `
 				<div class="gallery__item">
 					<a class="gallery__link" href="${original}">
 						<img
@@ -18,7 +18,35 @@ function galleryItemsGeneration(galleryItems) {
 						/>
 					</a>
 				</div>
-        	`;
-    })
-    .join('');
+        	`,
+		)
+		.join("");
+}
+
+galleryEl.addEventListener("click", onClick);
+
+function onClick(evt) {
+	evt.preventDefault();
+
+	if (evt.target.nodeName !== "IMG") {
+		return;
+	}
+
+	const instance = basicLightbox.create(
+		`
+		<img src="${evt.target.dataset.source}">
+	`,
+	);
+
+	instance.show();
+
+	galleryEl.addEventListener("keydown", onEscKeyPress);
+
+	function onEscKeyPress(evt) {
+		if (evt.code !== "Escape") {
+			return;
+		}
+		instance.close();
+		galleryEl.removeEventListener("keydown", onEscKeyPress);
+	}
 }
