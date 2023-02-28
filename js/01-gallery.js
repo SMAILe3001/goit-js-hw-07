75f3264fac21d2e553e14e422eb48a86dacecb87
SMAILe3001 <smaile3001@gmail.com>
2023-02-28 14:17:56 +0200

    first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsGeneration(galleryItems));
+
+function galleryItemsGeneration(galleryItems) {
+  return galleryItems
+    .map(({ preview, description, original }) => {
+      return `
+				<div class="gallery__item">
+					<a class="gallery__link" href="${original}">
+						<img
+						class="gallery__image"
+						src="${preview}"
+						data-source="${original}"
+						alt="${description}"
+						/>
+					</a>
+				</div>
+        	`;
+    })
+    .join('');
+}
